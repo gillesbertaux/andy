524a8d5ae70f6bfdf083e2cc8d39a2fd97a18abb
Gilles Bertaux <gillesbertauxdorgeville@gmail.com>
2014-11-19 12:08:01 +0100

    ignore js

--- js/andy.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$('.info').click(function() {
-		$('.modal').addClass('modal-show');
-		$('.modal-overlay').addClass('modal-show');
-	});
-	$('.modal-close').click(function() {
-	    $('.modal, .modal-overlay').removeClass('modal-show');
-	});
-});
